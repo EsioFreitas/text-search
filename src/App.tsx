bf19b510692ad0fca73a81a632da044c2e2b5caa
EsioFreitas <esio.gustavo@gmail.com>
2021-01-20 23:33:39 -0300

    (feat): add projetc html structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,27 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">text search</div>;
+  return (
+    <div>
+      <header>
+        <div>
+          <h1>Text Search</h1>
+          <div>
+            <input />
+            <button>Pesquisar</button>
+          </div>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <div /> Status
+            </li>
+          </ul>
+        </div>
+      </header>
+      <main>card list</main>
+    </div>
+  );
 }
 
 export default App;
