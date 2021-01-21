d05e93139551dcab7225892267890c6d149fc222
EsioFreitas <esio.gustavo@gmail.com>
2021-01-21 06:54:59 -0300

    (feat): add text search card component

---
+++ src/components/TextSearchCard/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles.css";
+
+const TextSearchCard: React.FC = () => {
+  return (
+    <div>
+      <h3>Titulo do card</h3>
+      <div>
+        <div>
+          <div /> Status
+        </div>
+        <button>Visualizar</button>
+      </div>
+    </div>
+  );
+};
+
+export default TextSearchCard;
