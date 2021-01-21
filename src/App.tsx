e612507ab27915d27cde32ead66bda1e22194d0e
EsioFreitas <esio.gustavo@gmail.com>
2021-01-21 08:55:10 -0300

    (feat): add error message

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,16 @@ import api from "./services/Api";
 function App() {
   const [searchString, setSearchString] = useState("");
   const [searchTexts, setSearchTexts] = useState<SearchText[]>([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const makeNewSearchText = (searchString: string) => {
-    api
-      .post("/crawl", { keyword: searchString })
-      .then(({ data }) => {
+  const makeNewSearchText = (searchString = "") => {
+    api.post("/crawl", { keyword: searchString }).then(({ data }) => {
+      if (data.status && data.status === 400) {
+        setErrorMessage(data.message);
+        setTimeout(function () {
+          setErrorMessage("");
+        }, 3000);
+      } else {
         setSearchTexts([
           ...searchTexts,
           {
@@ -21,35 +26,40 @@ function App() {
           },
         ]);
         setSearchString("");
-      })
-      .catch((error) => {});
+      }
+    });
   };
 
   return (
-    <div className="search-container">
-      <header className="search-header">
-        <h1>Text Search</h1>
-        <div className="search-input">
-          <input
-            type="text"
-            placeholder="Search..."
-            value={searchString}
-            onChange={(event) => setSearchString(event.target.value)}
-          />
-          <button onClick={() => makeNewSearchText(searchString)}>
-            search
-          </button>
-        </div>
-      </header>
-      <main className="search-list">
-        <h2>Text search list</h2>
-        <div>
-          {searchTexts.map((searchText, i) => (
-            <TextSearchCard key={i} searchText={searchText} />
-          ))}
-        </div>
-      </main>
-    </div>
+    <>
+      {errorMessage.length > 0 && (
+        <h3 className="error-message">{errorMessage}</h3>
+      )}
+      <div className="search-container">
+        <header className="search-header">
+          <h1>Text Search</h1>
+          <div className="search-input">
+            <input
+              type="text"
+              placeholder="Search..."
+              value={searchString}
+              onChange={(event) => setSearchString(event.target.value)}
+            />
+            <button onClick={() => makeNewSearchText(searchString)}>
+              search
+            </button>
+          </div>
+        </header>
+        <main className="search-list">
+          <h2>Text search list</h2>
+          <div>
+            {searchTexts.map((searchText, i) => (
+              <TextSearchCard key={i} searchText={searchText} />
+            ))}
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
 
