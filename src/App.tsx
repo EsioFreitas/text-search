ec09389d48e2acb9b8b2179549757ddef1122364
EsioFreitas <esio.gustavo@gmail.com>
2021-01-21 06:50:34 -0300

    (feat): style header application

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,12 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <header className="search-container">
+    <div className="search-container">
+      <header className="search-header">
         <h1>Text Search</h1>
-        <div>
-          <div>
-            <input />
-            <button>Pesquisar</button>
-          </div>
-          <ul>
-            <li>
-              <div /> Status
-            </li>
-          </ul>
+        <div className="search-input">
+          <input type="text" />
+          <button>Pesquisar</button>
         </div>
       </header>
       <main>
